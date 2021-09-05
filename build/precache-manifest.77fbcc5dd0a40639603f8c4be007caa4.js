500ac8cceb7d4073781f933e8907c311b0f58405
Diego <consultoresartificialbrain@gmail.com>
2021-09-05 18:48:36 -0500

    agregando a datos

---
+++ build/precache-manifest.77fbcc5dd0a40639603f8c4be007caa4.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "815e76812055916b70a55b72e22a627b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "413dd29b33240d00e90a",
+    "url": "/static/css/2.dbf7a291.chunk.css"
+  },
+  {
+    "revision": "650bac645ce8c9d4fb40",
+    "url": "/static/css/main.2151e22b.chunk.css"
+  },
+  {
+    "revision": "413dd29b33240d00e90a",
+    "url": "/static/js/2.9e773aca.chunk.js"
+  },
+  {
+    "revision": "842368a2e9efaefd5631cfc78081c546",
+    "url": "/static/js/2.9e773aca.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "650bac645ce8c9d4fb40",
+    "url": "/static/js/main.0c24255b.chunk.js"
+  },
+  {
+    "revision": "d1097b630b510257bae1",
+    "url": "/static/js/runtime-main.adaa668c.js"
+  },
+  {
+    "revision": "3c6258e568480fc1e8bd769e42d7579c",
+    "url": "/static/media/bg-tarjeta-01.3c6258e5.jpg"
+  },
+  {
+    "revision": "6611645bd370943ebc7139d4062b9377",
+    "url": "/static/media/chip-tarjeta.6611645b.png"
+  },
+  {
+    "revision": "5c60fb7f7e2f2116d67f629424b8525d",
+    "url": "/static/media/especial1.5c60fb7f.png"
+  },
+  {
+    "revision": "c53fde1537009f9cbaebc4cd586db01e",
+    "url": "/static/media/especial2.c53fde15.png"
+  },
+  {
+    "revision": "9fee3add372ea2fe987d6e82ec39f428",
+    "url": "/static/media/especial3.9fee3add.png"
+  },
+  {
+    "revision": "e4c091d3928afa83ab6cfecbce4a6a7f",
+    "url": "/static/media/estilo1.e4c091d3.png"
+  },
+  {
+    "revision": "39e535fe5ad51b057a54d57c85d5536e",
+    "url": "/static/media/estilo2.39e535fe.png"
+  },
+  {
+    "revision": "64c04b0669bc17254a4f9d83f74ba24c",
+    "url": "/static/media/estilo3.64c04b06.png"
+  },
+  {
+    "revision": "815bd5842cbaa75e72c455a876e7faf2",
+    "url": "/static/media/estilo4.815bd584.png"
+  },
+  {
+    "revision": "dfa26b4badbd181fdb8e0cfe5fc9e213",
+    "url": "/static/media/estilo5.dfa26b4b.png"
+  },
+  {
+    "revision": "5b57f8ec53509c02ad804fc4bbd34c88",
+    "url": "/static/media/estilo6.5b57f8ec.png"
+  },
+  {
+    "revision": "ae0915fe18dcc1bafdd686a15e448ed6",
+    "url": "/static/media/mastercard.ae0915fe.png"
+  },
+  {
+    "revision": "d2eb00b710fa2ee2c6a4408cb42f6fb4",
+    "url": "/static/media/visa.d2eb00b7.png"
+  }
+]);
